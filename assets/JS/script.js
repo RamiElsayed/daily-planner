c55ba00a304950d5047ce6a9b5e80a03df3be99d
Rami Elsayed <ramielsayed123@outlook.com>
2022-05-10 15:35:22 +0100

    redesigned table set color logic dynamically

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,14 +2,53 @@ const headerElem = document.getElementById("header");
 const h1Elem = document.getElementById("h1");
 const mainElem = document.getElementById("main");
 
-const dayDisplay = () => {
-      
-    const dayAndDate = document.createElement("h2");
-    dayAndDate.setAttribute("class","card-text");
-    dayAndDate.textContent = moment().format("MMMM Do YYYY")
-    console.log(dayAndDate.textContent);
-   
-    headerElem.append(dayAndDate);
-}    
+const renderDayAndDate = () => {
+  const dayAndDate = document.createElement("h2");
+  dayAndDate.classList.add("header-day-date");
+  dayAndDate.textContent = moment().format("MMMM Do YYYY");
+  
 
-dayDisplay()
+  headerElem.append(dayAndDate);
+};
+const renderDailyPlanner = () => {
+    const dailyTimeSlots = document.createElement("div");
+    dailyTimeSlots.classList.add("input-group");
+
+  for (let index = 6 ; index < 24; index++) {
+
+    const hourlySlot = document.createElement("div");
+    hourlySlot.classList.add("hourly-slot")
+    hourlySlot.classList.add(getTimeSlotClass(moment(), index));
+
+    const timeElem = document.createElement("span");
+    timeElem.classList.add("time-value");
+    timeElem.textContent = index;
+
+    const timeSlotElem = document.createElement("input");
+    timeSlotElem.setAttribute("type", "text");
+    timeSlotElem.classList.add("time-slot");
+    timeSlotElem.classList.add(getTimeSlotClass(moment(), index));
+    
+    hourlySlot.appendChild(timeElem);
+    hourlySlot.appendChild(timeSlotElem);
+    dailyTimeSlots.appendChild(hourlySlot);
+    mainElem.appendChild(dailyTimeSlots);
+  }
+};
+
+const getTimeSlotClass = (now, hour) => {
+  console.log(now.format("H"))
+  if (now.format("H") > hour) {
+    return 'past-time'
+  }
+  else if (now.format("H") < hour) {
+    return 'future-time'
+  }
+  return 'present-time';
+}
+
+const init = () => {
+  renderDayAndDate();
+  renderDailyPlanner();
+};
+init();
